a0e1278a530665dbfbcf79edb7f758e06034ffba
artem.kamenev <artkameneff@yandex.ru>
2023-11-07 16:24:05 +0300

    1.6 Аргументы

--- src/main/java/ru/calculator/ArgMethod.java
+++ src/main/java/ru/calculator/ArgMethod.java
@@ -9,5 +9,8 @@ public class ArgMethod {
         String name = "Java";
         int age = 6;
         ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
