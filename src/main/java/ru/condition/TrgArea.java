20dc5e6ad866e8476e40d86ba867247145b6e16f
artem.kamenev <artkameneff@yandex.ru>
2023-11-07 20:38:26 +0300

    1.13

---
+++ src/main/java/ru/condition/TrgArea.java
@@ -0,0 +1,13 @@
+package ru.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double rslP = (a + b + c) / 2;
+        return Math.sqrt(rslP * (rslP - a) * (rslP - b) * (rslP - c));
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
